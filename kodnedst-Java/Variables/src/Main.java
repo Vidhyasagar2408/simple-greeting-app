69bca2576f3cf63486e83ae6453bbc68631655c5
vidhyasagar <vs1522250@gmail.com>
2026-04-22 09:20:39 +0530

    pattern

---
+++ kodnedst-Java/Variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var age = 22;
+        var name = "vidhya";
+        var height = 5.10;
+        var is_married = false;
+
+        System.out.println("age = "+age);
+        System.out.println("name = "+name);
+        System.out.println("height = "+height);
+        System.out.println("is_married = "+is_married);
+    }
+}
